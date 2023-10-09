676c37ce6ec5c9b77bb80251d324a18b057bb015
fnakhsan <fn.akhsan@gmail.com>
2023-10-09 12:36:57 +0700

    submission2

--- core/src/main/java/com/example/core/di/DatabaseModule.kt
+++ core/src/main/java/com/example/core/di/DatabaseModule.kt
@@ -1,6 +1,7 @@
 package com.example.core.di
 
 import android.content.Context
+import androidx.annotation.Keep
 import androidx.room.Room
 import com.example.core.data.local.AnimeDao
 import com.example.core.data.local.AnimeDatabase
@@ -9,19 +10,26 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
+@Keep
 @Module
 @InstallIn(SingletonComponent::class)
 class DatabaseModule {
+    private val passphrase: ByteArray = SQLiteDatabase.getBytes("example".toCharArray())
+    private val factory = SupportFactory(passphrase)
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): AnimeDatabase = Room.databaseBuilder(
-        context,
-        AnimeDatabase::class.java,
-        "Anime.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): AnimeDatabase =
+        Room.databaseBuilder(
+            context,
+            AnimeDatabase::class.java,
+            "Anime.db"
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
+
 
     @Provides
     fun provideAnimeDao(database: AnimeDatabase): AnimeDao = database.animeDao()
--- fav/build.gradle.kts
+++ fav/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 apply("../shared_dependencies.gradle")
 android {
     namespace = "com.example.fav"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 24
@@ -40,7 +40,4 @@ android {
 dependencies {
     implementation(project(":app"))
     implementation(project(":core"))
-//    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-//    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-//    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 }
